7f806ec2dcc719a86a64d216f3505df3825a9bf2
jahsper Harrell <jahsper.harrell@gmail.com>
2023-05-09 20:45:18 -0600

    Added the usage section in the questions.

--- assets/index.js
+++ assets/index.js
@@ -7,47 +7,49 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'name of your project? (Required)'
+        message: 'name of your project?'
     },
     {
         type: 'input',
         name: 'description',
-        message: 'description of your project? (Required)'
+        message: 'description of your project?'
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'instructions for installation to run project. (Required)'
+        message: 'instructions for installation to run project.'
     },
     {
         type: 'list',
         name: 'license',
-        message: 'license for your project. (Required)',
+        message: 'license for your project.',
         choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
     },
     {
         type: 'input',
         name: 'constributing',
-        message: 'instructions for contributing to your project. (Required)'
+        message: 'instructions for contributing to your project.'
     },
     {
         type: 'input',
         name: 'tests',
-        message: 'instructions for testing your project. (Required)'
+        message: 'instructions for testing your project.'
     },
     {
         type: 'input',
         name: 'github',
-        message: 'github username for questions. (Required)'
-    }
+        message: 'github username for questions.'
+    },
     {
         type: 'input',
         name: 'email',
-        message: 'email address for questions. (Required)'
+        message: 'email address for questions.'
     },
-
-
-
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'usage information for your project.'
+    }
 ];
 
 // TODO: Create a function to write README file
