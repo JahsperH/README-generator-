e4dd6dea5c2e1d19892f8a43f6eaf743d0407e63
jahsper Harrell <jahsper.harrell@gmail.com>
2023-05-16 22:16:45 -0600

    added the license functions.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== "")
+  {
+    return '![GitHub license](https://img.shields.io/badge/license-'+license+'-blue.svg)'
+  }
+  else
+  {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+  if(license !== "")
+  {
+    return "https://opensource.org/licenses/"+license;
+  }
+  else
+  {
+    return "";
+  }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if(license !== "")
+  {
+    return "This project is licensed under the "+license+" license.";
+  }
+  else
+  {
+    return "";
+  }
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
